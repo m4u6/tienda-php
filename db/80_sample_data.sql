4ea15225b00eb9bab700f01af45b9cc3b61924b8
Mauricio Gonzalez <catmau@plytax.com>
2024-01-23 19:52:26 +0100

    added mysqli to docker image

--- db/80_sample_data.sql
+++ db/80_sample_data.sql
@@ -1,2 +1,2 @@
 USE tienda_db;
-INSERT INTO `clients` (`client_id`, `c_password`, `c_name`, `c_surname`, `c_phone`, `c_email`, `c_address`, `default_address`, `billing_address`) VALUES ('1', '9f103e782aa9af75ca33767f247d3c43a6b93ea699b650746611c89443b77624', 'mike', 'terry', '65646854', 'wsfion@asfdv.com', 'adfasdf', 'asdfasdf', 'gerge');
+INSERT INTO `users` (`user_id`, `u_password`, `u_name`, `u_surname`, `u_phone`, `u_email`, `u_address`, `default_address`, `billing_address`) VALUES ('1', '9f103e782aa9af75ca33767f247d3c43a6b93ea699b650746611c89443b77624', 'mike', 'terry', '65646854', 'wsfion@asfdv.com', 'adfasdf', 'asdfasdf', 'gerge');
