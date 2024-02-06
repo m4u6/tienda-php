7e2831131715e17928f67a9edc68f28f269925f3
Mauricio Gonzalez <catmau@plytax.com>
2024-02-06 07:43:10 +0100

    dasdf

--- html/index.php
+++ html/index.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'includes/config.php';
-require_once 'includes/db.php';
+require_once 'config/config.php';
+require_once 'config/db.php';
 define('PAGE_TITLE', 'Index');
 include 'includes/header.inc.php';
 
--- html/login.php
+++ html/login.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'includes/config.php';
-require_once 'includes/db.php';
+require_once 'config/config.php';
+require_once 'config/db.php';
 require_once 'includes/login.inc.php';
 
 define('PAGE_TITLE', 'Login');
