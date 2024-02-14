d5f36e935a60ae04114d7a1d00665365afa4a7ac
Mauricio Gonzalez <catmau@plytax.com>
2024-02-14 23:06:09 +0100

    sortable tables now work (kinda)

--- php-shop/views/dashboard/view.products.dashboard.php
+++ php-shop/views/dashboard/view.products.dashboard.php
@@ -2,7 +2,8 @@
 <div class="container-fluid">
     <h1 class="mt-4">Productos</h1>
     <a href="?edit=new">Nuevo producto</a>
-    <?php var_dump(query_products($conn, $query1)); 
+    <?php 
+    #var_dump(query_products($conn, $query1)); 
     
     sortable_table(query_products($conn, $query1), $no_sort_array);
     
--- php-shop/views/view.f.sortable_table.php
+++ php-shop/views/view.f.sortable_table.php
@@ -5,52 +5,42 @@ function sortable_table($data, $no_sort_array) {
     # $data should be a multidimiensional array containing all rows ($data[$row_number][$column_name])
     # $no_sort_array is an array containing the column_names that shouldn't be sorted
     # https://www.w3.org/WAI/ARIA/apg/patterns/table/examples/sortable-table/
-    ?>
-    <div class="table-wrap">
-        <table class="sortable">
-            <thead>
-                <tr>
-    <?php
+
+    echo "<div class=\"table-wrap\">";
+    echo "<table class=\"sortable\">";
+    echo "<thead>";
+    echo "<tr>";
     foreach ($data[1] as $col_name => $value) {
-        ?>
-        <th <?php
             $classes = "";
-            if (in_array($col_name, $no_sort_array)) {
+            if (in_array($col_name, $no_sort_array)) {  # Si el nombre del th esta en el array $no_sort_array añadiremos la clase no-sort
                 $classes .= "no-sort ";
             }
             if (preg_match('/^[\d]+[.]?[\d]*$/', $value) === 1) { # si es un numero añadimos la clase num
                 $classes .= "num ";
             }
             if ($classes != "") {
-                echo "class=\"$classes\">";
-            }?>
-          <button>
-            <?php echo $col_name ?>
-            <span aria-hidden="true"></span>
-          </button>
-        </th>
-        <?php
-    }
-
-
-    ?>
-                </tr>
-            </thead>
-            <tbody>
-    <?php
+                echo "<th class=\"$classes\" style=\"height:30px\">";   # Un apaño lo de style pero bueno
+            } else {
+                echo "<th>";
+            }
+            echo "<button>$col_name";
+            echo "<span aria-hidden=\"true\"></span>";
+            echo "</button>";
+            echo "</th>";
+        }
+    echo "</tr>";
+    echo "</thead>";
+    echo "<tbody>";
     foreach ($data as $row) {
         echo "<tr>";
         foreach ($row as $col_name => $value) {
             echo (preg_match('/^[\d]+[.]?[\d]*$/', $value) === 1) ? "<td class=\"num\">" : "<td>";
             echo "$value</td>";
         }
-        echo "<tr>";
+        echo "</tr>";
     }
-    ?>
-            </tbody>
-        </table>
-    </div>
-    <?php
-
+    echo "</tbody>";
+    echo "</table>";  
+    echo "</div>";
 }
 ?>
