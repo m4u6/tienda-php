e6d7bde9c30091f15776aee041980267ade2012c
Mauricio Gonzalez <catmau@plytax.com>
2024-02-19 18:19:10 +0100

    minor db fix

--- db/00_init.sql
+++ db/00_init.sql
@@ -119,6 +119,7 @@ CREATE TABLE IF NOT EXISTS orders-products (
     order_product_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     product_id INT,
+    price_per_unit DECIMAL(10, 2),
     quantity INT,
     FOREIGN KEY (order_id) REFERENCES orders(order_id),
     FOREIGN KEY (product_id) REFERENCES products(product_id)
